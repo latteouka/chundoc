ebbdf1764c8d8324ee0def75bd12633ed86ba221
oukalatte <oukalatte@gmail.com>
2023-05-03 13:54:15 +0800

    update

--- components/blog.tsx
+++ components/blog.tsx
@@ -19,11 +19,6 @@ const Box = ({ blog }: { blog: BlogType }) => {
       <div className={style.box}>
         <div className={style.title}>{blog.title}</div>
         <div className={style.desc}>{blog.desc}</div>
-        <div className={style.tags}>
-          {blog.tags.map((tag) => {
-            return <div className={style.tag}>{tag}</div>;
-          })}
-        </div>
         <div className={style.date}>{blog.date}</div>
       </div>
     </Link>
