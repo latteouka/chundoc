14756b875593f662a5c1f3bc2f81f4701b92c978
oukalatte <oukalatte@gmail.com>
2023-07-06 15:33:55 +0800

    update

--- pages/draw.tsx
+++ pages/draw.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./draw.module.scss";
 
 const title = "luckydraw1";
@@ -8,6 +8,7 @@ const title = "luckydraw1";
 const Draw = () => {
   const [email, setEmail] = useState("");
   const [done, setDone] = useState(false);
+  const [ready, setReady] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const router = useRouter();
@@ -15,16 +16,39 @@ const Draw = () => {
 
   async function handleSubmit() {
     setLoading(true);
-    await axios.post("https://jpbox.chundev.com/draws/add", {
-      userId: params.userId,
-      platform: params.platform,
-      title,
-      email,
-      createdAt: new Date(),
-    });
+    if (email === "") {
+      await axios.post("https://jpbox.chundev.com/draws/add", {
+        userId: params.userId,
+        platform: params.platform,
+        title,
+        email,
+        createdAt: new Date(),
+      });
+    } else {
+      await axios.post("https://jpbox.chundev.com/draws/update", {
+        userId: params.userId,
+        platform: params.platform,
+        title,
+        email,
+        createdAt: new Date(),
+      });
+    }
     setLoading(false);
     setDone(true);
   }
+
+  useEffect(() => {
+    if (!params.userId) return;
+    const getNow = async () => {
+      const result = await axios.post("http://localhost:4000/draws/now", {
+        userId: params.userId,
+      });
+      if (!result.data) return;
+      setEmail(result.data.email);
+    };
+
+    getNow();
+  }, [params.userId]);
 
   if (!params.userId || !params.platform) return null;
 
@@ -47,7 +71,7 @@ const Draw = () => {
         onClick={handleSubmit}
         disabled={loading}
       >
-        Submit
+        Update
       </button>
     </div>
   );
